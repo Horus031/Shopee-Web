e21289ffb03faa0da2f5687b648da3056072bbdb
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2024-10-07 22:34:48 +0700

    hotro

--- components/hotro.vue
+++ components/hotro.vue
@@ -1,18 +1,15 @@
 <template>
-    <div clas="cont d-flex">
-        <v-sheet class="small d-flex">
-            <p></p>
-            <v-card>
-            </v-card>
-            <div></div>
-            <div>Danh mục</div>
-            <div>
-                <v-card>
-                    <v-card-title>
-                        asndaondw
-                    </v-card-title>
-                </v-card>
-            </div>        
+    <div class="cont d-flex" style="justify-content: center; display: flex; flex-direction: column;">
+        <v-card style="background-color: #ee4d2d ; display: flex;flex-direction: column;" class="real" elevation="0">
+            <v-card-title class="pa-0 ma-0" style="padding:0px; justify-content: center;">
+                <div>
+                    <p style="color: white; padding-bottom: 50px;padding-top: 25px; font-size: 36px;">Xin chào shopee có thể giúp gì cho bạn?</p>
+                    <p class="gg" style="padding-left:0px ;">nhập từ khoá hoặc nội dung cần tìm</p> 
+                </div>
+            </v-card-title>    
+        </v-card>
+        <v-sheet class="small" style="justify-content: center; display: flex;">
+            danh mục
         </v-sheet>
     </div>
 </template>
@@ -21,13 +18,30 @@
 
 </script>
 <style>
+
+.real{
+    min-width: 1530px;
+    max-width: 1530px;
+    font-family: Roboto, system-ui, -apple-system, BlinkMacSystemFont, PingFang SC, Source Han Sans CN-Normal, Noto Sans CJK SC, Segoe UI, Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
+}
 .cont{
     min-width: 1530px;
     max-width: 1530px;
-    justify-content: center;
+    font-family: Roboto, system-ui, -apple-system, BlinkMacSystemFont, PingFang SC, Source Han Sans CN-Normal, Noto Sans CJK SC, Segoe UI, Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
+
+}
+.gg{
+    max-width: 81%;
+    min-width: 81%;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 59px;
+    font-family: Roboto, system-ui, -apple-system, BlinkMacSystemFont, PingFang SC, Source Han Sans CN-Normal, Noto Sans CJK SC, Segoe UI, Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
 }
 .small{
     min-width: 81%;
     max-width: 81%;
+    font-family: Roboto, system-ui, -apple-system, BlinkMacSystemFont, PingFang SC, Source Han Sans CN-Normal, Noto Sans CJK SC, Segoe UI, Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
+
 }
 </style>
