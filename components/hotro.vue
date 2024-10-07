f4fda1edbab192174d049da62380be04c3df23e0
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2024-10-07 18:36:07 +0700

    gg

--- components/hotro.vue
+++ components/hotro.vue
@@ -1,21 +1,33 @@
 <template>
-    <v-sheet>
-        <p></p>
-        <v-card>
-
-        </v-card>
-        <div></div>
-        <div>Danh mục</div>
-        <div>
+    <div clas="cont d-flex">
+        <v-sheet class="small d-flex">
+            <p></p>
             <v-card>
-                <v-card-title>
-                    asndaondw
-                </v-card-title>
             </v-card>
-        </div>        
-    </v-sheet>
+            <div></div>
+            <div>Danh mục</div>
+            <div>
+                <v-card>
+                    <v-card-title>
+                        asndaondw
+                    </v-card-title>
+                </v-card>
+            </div>        
+        </v-sheet>
+    </div>
 </template>
 
 <script>
 
 </script>
+<style>
+.cont{
+    min-width: 1530px;
+    max-width: 1530px;
+    justify-content: center;
+}
+.small{
+    min-width: 81%;
+    max-width: 81%;
+}
+</style>
